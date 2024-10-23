516d9d714896fd7305eef5828a9349236b95cbc4
Mattt Zmuda <mattt@loopwork.ai>
2024-10-23 09:45:17 -0700

    Add RPC example

---
+++ examples/rpc.py
@@ -0,0 +1,41 @@
+"""
+This example shows how to serve a function over HTTP with Hype.
+
+Download `uv` to run this example: https://github.com/astral-sh/uv
+
+```
+uv run examples/rpc.py
+```
+"""
+
+import json
+import logging
+
+from fastapi.testclient import TestClient
+
+import hype
+
+
+@hype.up
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(levelname)s [%(asctime)s] %(name)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )
+
+    app = hype.create_fastapi_app([add])
+
+    with TestClient(app) as client:
+        response = client.get("/openapi.json")
+        print("OpenAPI schema:")
+        print(json.dumps(response.json(), indent=2))
+
+        print()
+
+        response = client.post("/add", json={"x": 1, "y": 2})
+        print(f"Response: {response.json()}")
